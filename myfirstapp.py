d750028a6e5c351ccdab8d84dd48d2fe5fb717ba
aaa-yanbin <94594122+aaa-yanbin@users.noreply.github.com>
2022-03-19 16:59:00 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -14,10 +14,10 @@ def user_input_features():
     sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
     petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
     petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-    data = {'sepal_length': sepal_length,
-            'sepal_width': sepal_width,
-            'petal_length': petal_length,
-            'petal_width': petal_width}
+    data = {'SepalLengthCm': sepal_length,
+            'SepalWidthCm': sepal_width,
+            'PetalLengthCm': petal_length,
+            'PetalWidthCm': petal_width}
     features = pd.DataFrame(data, index=[0])
     return features
 
@@ -38,10 +38,10 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(['setosa', 'versicolor', 'virginica'])
+st.write(pd.DataFrame(['setosa', 'versicolor', 'virginica']))
 
 st.subheader('Prediction')
-st.write(['setosa', 'versicolor', 'virginica'][prediction])
+st.write(prediction)
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
